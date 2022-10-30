c188af7fd794992cfcb67ef0b3112adf52c79dff
d4vvv <kacper.godula99@gmail.com>
2022-10-30 14:54:44 +0100

    feat: add tailwindcss to the project

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,7 +26,7 @@ export default function Home() {
             <h2>Documentation &rarr;</h2>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
-
+          <p className="text-red-500">TEST</p>
           <a href="https://nextjs.org/learn" className={styles.card}>
             <h2>Learn &rarr;</h2>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
