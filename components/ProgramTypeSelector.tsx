88b69bf89839609bfc48e9635aaa102bfa65e22f
d4vvv <kacper.godula99@gmail.com>
2022-10-31 14:44:32 +0100

    refactor: remove unnecessary useState import in ProgramTypeSelector

--- components/ProgramTypeSelector.tsx
+++ components/ProgramTypeSelector.tsx
@@ -1,5 +1,5 @@
 import { Checkbox, Flex, useCheckboxGroup } from '@chakra-ui/react'
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { ProgramType } from '../types/ProgramType'
 
 interface ProgramTypeSelectorProps {
